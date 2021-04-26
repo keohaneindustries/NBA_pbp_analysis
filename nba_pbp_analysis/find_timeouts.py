8c9ac30933807e8a0f7fdccd8f638b76cac80f19
Luke Keohane <keohane.industries@gmail.com>
2021-04-25 19:28:40 -0500

    fixed bug

--- nba_pbp_analysis/find_timeouts.py
+++ nba_pbp_analysis/find_timeouts.py
@@ -18,7 +18,7 @@ def get_two_minute_outcomes(years_requested: list) -> pd.DataFrame:
     ]
     df = load_clean_pbp(clean_dir=CLEAN_ROOT_DIR, filetype=CLEAN_OUTPUT_FILETYPE, years_requested=years_requested,
                         filter_fxs=_filter_fxs)
-    
+
     # awaydesc = df['AWAYDESCRIPTION'].dropna()
     # awaytimeouts = awaydesc[awaydesc.str.contains('Time')]
     # timeout_type = df['TIMEOUT_TYPE'].dropna()
@@ -27,30 +27,27 @@ def get_two_minute_outcomes(years_requested: list) -> pd.DataFrame:
         df[df['remaining_in_quarter'] >= pd.datetime.today().replace(hour=2, minute=0, second=0)].groupby('game_id')[
             'remaining_in_quarter'].last().reset_index()
     last_play_before_2_mins['last_play_before_2_mins'] = True
-    
+
     df = df.merge(last_play_before_2_mins, on=['game_id', 'remaining_in_quarter'], how='left')
-    
+
     df['last_play_before_2_mins'].fillna(False, inplace=True)
-    df['SCOREMARGIN'] = df['SCOREMARGIN'].ffill()
-    df['SCOREMARGIN'] = df['SCOREMARGIN'].bfill()
-    df['SCOREMARGIN'] = df['SCOREMARGIN'].replace('TIE', 0)
-    df['SCOREMARGIN'] = df['SCOREMARGIN'].astype(int)
-    
+    df['SCOREMARGIN'] = (df['home_score'] - df['away_score'].astype(np.int)).astype(np.int)
+
     final_home_margin = df.groupby('game_id')['SCOREMARGIN'].last().reset_index()
     final_margin_ix_game = final_home_margin.rename(columns={'SCOREMARGIN': 'final_score_margin'}).set_index('game_id')
-    
+
     two_mins_home_margin = \
         df[df['last_play_before_2_mins']].drop_duplicates(subset='game_id', keep='last').set_index('game_id')[
             'SCOREMARGIN'].rename('two_mins_score_margin')
-    
+
     two_min_outcomes = final_margin_ix_game.join(two_mins_home_margin, how='inner')
-    
+
     two_min_outcomes['winning_team'] = np.where(
         two_min_outcomes['final_score_margin'] > 0,
         'home',
         'away'
     )
-    
+
     two_min_outcomes['winning_team_two_min_margin'] = np.where(
         two_min_outcomes['final_score_margin'] > 0,
         two_min_outcomes['two_mins_score_margin'],
@@ -75,4 +72,7 @@ _years_requested = [
 
 agg_two_min_outcomes = pd.concat([get_two_minute_outcomes(years_requested=years) for years in _years_requested])
 
-agg_two_min_outcomes.groupby('abs_two_mins_score_margin')['leading_team_won'].mean()
+win_rate_by_margin = agg_two_min_outcomes.groupby('abs_two_mins_score_margin')['leading_team_won'].mean()
+print(win_rate_by_margin)
+
+win_rate_by_margin.to_csv("win_rate_by_two_min_margin.csv")
