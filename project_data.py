b70cf5861168ee9373569ee22e1ebca91cd1cc37
Luke Keohane <keohane.industries@gmail.com>
2021-04-29 17:54:06 -0500

    added remaining To-Do's

--- project_data.py
+++ project_data.py
@@ -30,6 +30,22 @@ def main():
     df_close_games = requester.get_data(data_sourcer=CloseGamesByGameIDData(local_io=local_io),
                                         years_requested=_years_requested)
     
+    # ANOVA data: index=playid
+    # TODO get all plays last 3 mins
+    # TODO filter to "legit" plays (e.g. not substitutions) - make sure to retain timeouts for next step
+    # TODO flag offensive plays immediately following timeouts (append bool; not filter)
+    # TODO calc for each play: points scored on play (for team that called timeout)
+    # TODO left join close game; filter to close game == True
+    
+    # Classifier data: index=playid
+    # TODO get all plays last 3 mins
+    # TODO calc for each play: point margin, time remaining (s), num of timeouts remaining (home, away)
+    # TODO drop duplicates
+    # TODO left join close game; filter to close game == True
+    # TODO left join winners;
+    # TODO split into home/away, apply any necessary transformations to align variables, then stack
+    # TODO calc for each play "did eventually win game" (bool)
+    
     return
 
 
