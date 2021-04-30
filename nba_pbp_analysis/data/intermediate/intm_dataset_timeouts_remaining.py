e71e87d4332c8923c9cb4a22f5e2d4e63ab4a0e7
Luke Keohane <keohane.industries@gmail.com>
2021-04-30 17:04:34 -0500

    changed indexing; cleaned up formatting

--- nba_pbp_analysis/data/intermediate/intm_dataset_timeouts_remaining.py
+++ nba_pbp_analysis/data/intermediate/intm_dataset_timeouts_remaining.py
@@ -10,7 +10,7 @@ from nba_pbp_analysis.data.intermediate.base_intm_dataset import BaseIntermediat
 
 class TimeoutsRemainingData(BaseIntermediateDataIO):
     request_id = "timeoutsremaining_playid"
-
+    
     @classmethod
     def _filter_fxs(cls, **kwargs) -> list:
         return [
@@ -18,7 +18,7 @@ class TimeoutsRemainingData(BaseIntermediateDataIO):
             # lambda df: df[pd.to_datetime(df['remaining_in_quarter']) <= max_remaining_in_quarter]
             # lambda df: df[pd.to_datetime(df['remaining_in_quarter'], format="%M:%S") <= max_rem_in_quarter]
         ]
-
+    
     @classmethod
     def source(cls, years_requested: list, **kwargs) -> pd.DataFrame:
         ## input
@@ -26,14 +26,15 @@ class TimeoutsRemainingData(BaseIntermediateDataIO):
         df = cls.read_raw_data(years_requested=years_requested, **kwargs)
         # slice to minimum variables required
         df = cls._slice_to_relevant_vars_for_timeout_remaining_backfill(df=df)
-
+        
         ## calcs
         # calc timeouts remaining for each play
+        # TODO troubleshoot df_timeouts_remaining
         df = cls._calc_timeouts_remaining(df=df)
-
+        
         # df = cls._calc_home_timeouts_remaining(df=df)
         # df = cls._calc_away_timeouts_remaining(df=df)
-
+        
         ## output
         # slice to minimum timespan required in final output
         max_rem_in_quarter = dt.datetime(year=1900, month=1, day=1, hour=0, minute=3, second=0)
@@ -42,24 +43,26 @@ class TimeoutsRemainingData(BaseIntermediateDataIO):
         df = cls._rename_vars(df=df)
         # slice to minimum variables required in output
         df = cls._slice_to_output_vars(df=df)
-
+        # set index for uniform .csv file I/O
+        df = cls._reindex_game_play_id(df=df)
+        
         return df
-
+    
     @staticmethod
     def _slice_to_relevant_vars_for_timeout_remaining_backfill(df: pd.DataFrame) -> pd.DataFrame:
         relevant_vars = ['game_id', 'play_id', 'HOMEDESCRIPTION', 'AWAYDESCRIPTION', 'rem_in_quarter_dt']
         return df.loc[:, relevant_vars]
-
+    
     @classmethod
     def _calc_timeouts_remaining(cls, df: pd.DataFrame) -> pd.DataFrame:
         l_games = []
-
+        
         for i in df['game_id'].unique()[0:2]:
             game = df[df['game_id'] == i].copy()
-
+            
             game['HOMETIMEOUTS'] = np.nan
             game['HOMETIMEOUTS'][game['play_id'] == game['play_id'].min()] = 3
-
+            
             count = 0
             timeouts_remaining = 3
             for i in range(0, len(game)):
@@ -69,12 +72,12 @@ class TimeoutsRemainingData(BaseIntermediateDataIO):
                         game.iloc[i, -1] = timeouts_remaining
                 except:
                     game.iloc[i, -1] = timeouts_remaining
-
+            
             game['HOMETIMEOUTS'].fillna(method='ffill', inplace=True)
-
+            
             game['AWAYTIMEOUTS'] = np.nan
             game['AWAYTIMEOUTS'][game['play_id'] == game['play_id'].min()] = 3
-
+            
             count = 0
             timeouts_remaining = 3
             for i in range(0, len(game)):
@@ -84,39 +87,46 @@ class TimeoutsRemainingData(BaseIntermediateDataIO):
                         game.iloc[i, -1] = timeouts_remaining
                 except:
                     game.iloc[i, -1] = timeouts_remaining
-
+            
             game['AWAYTIMEOUTS'].fillna(method='ffill', inplace=True)
-
+            
             l_games.append(game.copy())
-
+        
         df_games = pd.concat(l_games, ignore_index=True)
-
+        
         # df_games = df_games.join(pd.get_dummies(df_games['HOMETIMEOUTS'], prefix='HOME_TIMEOUT'))
         # df_games = df_games.join(pd.get_dummies(df_games['AWAYTIMEOUTS'], prefix='AWAY_TIMEOUT'))
         # df_games.drop(['HOMETIMEOUTS', 'AWAYTIMEOUTS'], axis=1, inplace=True)
-
+        
         return df_games
-
+    
     @classmethod
     def _calc_home_timeouts_remaining(cls, df: pd.DataFrame) -> pd.DataFrame:
         # TODO _calc_home_timeouts_remaining
         return df
-
+    
     @classmethod
     def _calc_away_timeouts_remaining(cls, df: pd.DataFrame) -> pd.DataFrame:
         # TODO _calc_away_timeouts_remaining
         return df
-
+    
     @staticmethod
     def _slice_to_relevant_timespan(df: pd.DataFrame, max_rem_in_quarter: dt.datetime) -> pd.DataFrame:
         return df[df['rem_in_quarter_dt'] <= max_rem_in_quarter]
-
+    
     @staticmethod
     def _rename_vars(df: pd.DataFrame) -> pd.DataFrame:
         var_map = {'HOMETIMEOUTS': 'timeouts_rem_home', 'AWAYTIMEOUTS': 'timeouts_rem_away'}
         return df.rename(columns=var_map)
-
+    
     @staticmethod
     def _slice_to_output_vars(df: pd.DataFrame) -> pd.DataFrame:
         relevant_vars = ['game_id', 'play_id', 'timeouts_rem_home', 'timeouts_rem_away']
         return df.loc[:, relevant_vars]
+    
+    @staticmethod
+    def _reindex_game_play_id(df: pd.DataFrame) -> pd.DataFrame:
+        df['game_play_id'] = df[['game_id', 'play_id']].astype(np.int).apply(
+            lambda row: "{}_{}".format(row['game_id'], row['play_id']), axis=1)
+        df.set_index('game_play_id', inplace=True)
+        return df
