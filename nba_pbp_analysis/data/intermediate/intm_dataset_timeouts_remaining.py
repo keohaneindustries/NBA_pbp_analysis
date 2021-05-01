8e2584eea3fb074189e1ef90b6e726af8e5867ec
Luke Keohane <keohane.industries@gmail.com>
2021-04-30 19:04:09 -0500

    re-wrote TimeoutsRemaining

--- nba_pbp_analysis/data/intermediate/intm_dataset_timeouts_remaining.py
+++ nba_pbp_analysis/data/intermediate/intm_dataset_timeouts_remaining.py
@@ -14,13 +14,13 @@ class TimeoutsRemainingData(BaseIntermediateDataIO):
     @classmethod
     def _filter_fxs(cls, **kwargs) -> list:
         return [
-            lambda df: df[df['period'] == 4]
+            lambda df: df[(df['period'] == 3) | (df['period'] == 4)]
             # lambda df: df[pd.to_datetime(df['remaining_in_quarter']) <= max_remaining_in_quarter]
             # lambda df: df[pd.to_datetime(df['remaining_in_quarter'], format="%M:%S") <= max_rem_in_quarter]
         ]
     
     @classmethod
-    def source(cls, years_requested: list, **kwargs) -> pd.DataFrame:
+    def source(cls, years_requested: list, n_timeouts_allowed: int = 8, **kwargs) -> pd.DataFrame:
         ## input
         # get all plays 4th quarter
         df = cls.read_raw_data(years_requested=years_requested, **kwargs)
@@ -29,18 +29,12 @@ class TimeoutsRemainingData(BaseIntermediateDataIO):
         
         ## calcs
         # calc timeouts remaining for each play
-        # TODO troubleshoot df_timeouts_remaining
-        df = cls._calc_timeouts_remaining(df=df)
-        
-        # df = cls._calc_home_timeouts_remaining(df=df)
-        # df = cls._calc_away_timeouts_remaining(df=df)
+        df = cls._calc_timeouts_remaining(df=df, n_timeouts_allowed=n_timeouts_allowed)
         
         ## output
         # slice to minimum timespan required in final output
         max_rem_in_quarter = dt.datetime(year=1900, month=1, day=1, hour=0, minute=3, second=0)
-        df = cls._slice_to_relevant_timespan(df=df, max_rem_in_quarter=max_rem_in_quarter)
-        # rename variables
-        df = cls._rename_vars(df=df)
+        df = cls._slice_to_relevant_timespan(df=df, max_rem_in_quarter=max_rem_in_quarter, period=4)
         # slice to minimum variables required in output
         df = cls._slice_to_output_vars(df=df)
         # set index for uniform .csv file I/O
@@ -50,78 +44,49 @@ class TimeoutsRemainingData(BaseIntermediateDataIO):
     
     @staticmethod
     def _slice_to_relevant_vars_for_timeout_remaining_backfill(df: pd.DataFrame) -> pd.DataFrame:
-        relevant_vars = ['game_id', 'play_id', 'HOMEDESCRIPTION', 'AWAYDESCRIPTION', 'rem_in_quarter_dt']
+        relevant_vars = ['game_id', 'play_id', 'HOMEDESCRIPTION', 'AWAYDESCRIPTION', 'period', 'rem_in_quarter_dt']
         return df.loc[:, relevant_vars]
     
     @classmethod
-    def _calc_timeouts_remaining(cls, df: pd.DataFrame) -> pd.DataFrame:
-        l_games = []
+    def _calc_timeouts_remaining(cls, df: pd.DataFrame, n_timeouts_allowed: int = 8) -> pd.DataFrame:
+        home_timeouts = cls._calc_timeouts_used_by_side(df=df, side="home", n_timeouts_allowed=n_timeouts_allowed)
+        away_timeouts = cls._calc_timeouts_used_by_side(df=df, side="away", n_timeouts_allowed=n_timeouts_allowed)
         
-        for i in df['game_id'].unique()[0:2]:
-            game = df[df['game_id'] == i].copy()
-            
-            game['HOMETIMEOUTS'] = np.nan
-            game['HOMETIMEOUTS'][game['play_id'] == game['play_id'].min()] = 3
-            
-            count = 0
-            timeouts_remaining = 3
-            for i in range(0, len(game)):
-                try:
-                    if game.iloc[i, 2].lower().find('timeout') != -1:
-                        timeouts_remaining -= 1
-                        game.iloc[i, -1] = timeouts_remaining
-                except:
-                    game.iloc[i, -1] = timeouts_remaining
-            
-            game['HOMETIMEOUTS'].fillna(method='ffill', inplace=True)
-            
-            game['AWAYTIMEOUTS'] = np.nan
-            game['AWAYTIMEOUTS'][game['play_id'] == game['play_id'].min()] = 3
-            
-            count = 0
-            timeouts_remaining = 3
-            for i in range(0, len(game)):
-                try:
-                    if game.iloc[i, 3].lower().find('timeout') != -1:
-                        timeouts_remaining -= 1
-                        game.iloc[i, -1] = timeouts_remaining
-                except:
-                    game.iloc[i, -1] = timeouts_remaining
-            
-            game['AWAYTIMEOUTS'].fillna(method='ffill', inplace=True)
-            
-            l_games.append(game.copy())
-        
-        df_games = pd.concat(l_games, ignore_index=True)
-        
-        # df_games = df_games.join(pd.get_dummies(df_games['HOMETIMEOUTS'], prefix='HOME_TIMEOUT'))
-        # df_games = df_games.join(pd.get_dummies(df_games['AWAYTIMEOUTS'], prefix='AWAY_TIMEOUT'))
-        # df_games.drop(['HOMETIMEOUTS', 'AWAYTIMEOUTS'], axis=1, inplace=True)
-        
-        return df_games
+        timeouts = df[['game_id', 'play_id', 'period', 'rem_in_quarter_dt']].join(home_timeouts).join(away_timeouts)
+        timeouts = timeouts.set_index('game_id', append=True).groupby(level=1).ffill().reset_index(level=1)
+        return timeouts
     
     @classmethod
-    def _calc_home_timeouts_remaining(cls, df: pd.DataFrame) -> pd.DataFrame:
-        # TODO _calc_home_timeouts_remaining
-        return df
+    def _calc_timeouts_used_by_side(cls, df: pd.DataFrame, side: str = "home",
+                                    n_timeouts_allowed: int = 8) -> pd.DataFrame:
+        if side == "home":
+            col_name = "HOMEDESCRIPTION"
+        elif side == "away":
+            col_name = "AWAYDESCRIPTION"
+        else:
+            raise ValueError
+        str_timeouts = cls._find_timeouts(df=df, col_name=col_name)
+        n_timeouts = str_timeouts[col_name].str[10:].str.extract('(\d)(?:\D+)(\d)').fillna(-1).astype(np.int)
+        n_timeouts.columns = ['to_n_full', 'to_n_short']
+        n_timeouts['to_n_total'] = n_timeouts['to_n_full'] + n_timeouts['to_n_short']
+        n_timeouts['to_rem'] = (n_timeouts['to_n_total'] - n_timeouts_allowed) * (-1)
+        n_timeouts[n_timeouts['to_n_total'] < 0] = np.nan
+        n_timeouts.columns = [side + "_" + col for col in n_timeouts.columns]
+        return n_timeouts.copy()
     
     @classmethod
-    def _calc_away_timeouts_remaining(cls, df: pd.DataFrame) -> pd.DataFrame:
-        # TODO _calc_away_timeouts_remaining
-        return df
+    def _find_timeouts(cls, df: pd.DataFrame, col_name: str) -> pd.DataFrame:
+        return df[df[col_name].str.contains("Timeout").fillna(False)]
     
     @staticmethod
-    def _slice_to_relevant_timespan(df: pd.DataFrame, max_rem_in_quarter: dt.datetime) -> pd.DataFrame:
-        return df[df['rem_in_quarter_dt'] <= max_rem_in_quarter]
-    
-    @staticmethod
-    def _rename_vars(df: pd.DataFrame) -> pd.DataFrame:
-        var_map = {'HOMETIMEOUTS': 'timeouts_rem_home', 'AWAYTIMEOUTS': 'timeouts_rem_away'}
-        return df.rename(columns=var_map)
+    def _slice_to_relevant_timespan(df: pd.DataFrame, max_rem_in_quarter: dt.datetime, period: int = 4) -> pd.DataFrame:
+        return df[(df['rem_in_quarter_dt'] <= max_rem_in_quarter) & (df['period'] == period)].dropna()
     
     @staticmethod
     def _slice_to_output_vars(df: pd.DataFrame) -> pd.DataFrame:
-        relevant_vars = ['game_id', 'play_id', 'timeouts_rem_home', 'timeouts_rem_away']
+        op_col_suffixes = ['_to_n_full', '_to_n_short', '_to_n_total', '_to_rem']
+        relevant_vars = ['game_id', 'play_id'] + ["home" + suf for suf in op_col_suffixes] + ["away" + suf for suf in
+                                                                                              op_col_suffixes]
         return df.loc[:, relevant_vars]
     
     @staticmethod
--- project_data.py
+++ project_data.py
@@ -18,8 +18,8 @@ _GLOBAL_SAVE_INTERMEDIATE_FILES_CSV = True
 # %% main
 def main():
     # _years_requested = list(range(2008, 2019))
-    # _years_requested = list(range(2008, 2009))
-    _years_requested = list(range(2008, 2011))
+    _years_requested = list(range(2008, 2009))
+    # _years_requested = list(range(2008, 2011))
 
     local_io = LocalIOUtils
     # local_io.save_dirpath = GLOBAL_SAVE_DIRPATH
@@ -28,23 +28,23 @@ def main():
     # requester.save_final_file = _GLOBAL_SAVE_FINAL_FILES_CSV
     # requester.save_int_files = _GLOBAL_SAVE_INTERMEDIATE_FILES_CSV
 
-    df_winners = requester.get_data(data_sourcer=WinnerData(local_io=local_io), years_requested=_years_requested)
-    df_close_games = requester.get_data(data_sourcer=CloseGamesByGameIDData(local_io=local_io),
-                                        years_requested=_years_requested)
+    # df_winners = requester.get_data(data_sourcer=WinnerData(local_io=local_io), years_requested=_years_requested)
+    # df_close_games = requester.get_data(data_sourcer=CloseGamesByGameIDData(local_io=local_io),
+    #                                     years_requested=_years_requested)
     df_timeouts_remaining = requester.get_data(data_sourcer=TimeoutsRemainingData(local_io=local_io),
-                                               years_requested=_years_requested)
+                                               years_requested=_years_requested, force_source=True)
 
-    # TODO ANOVA data: index=playid
+    # TODO ANOVA data: index=obs_id
     # TODO get all plays last 3 mins
     # TODO filter to "legit" plays (e.g. not substitutions) - make sure to retain timeouts for next step
     # TODO flag offensive plays immediately following timeouts (append bool; not filter)
     # TODO calc for each play: points scored on play (for team that called timeout)
     # TODO left join close game; filter to close game == True
     # TODO
-    # TODO Classifier data: index=playid
-    df_classifier_data = requester.get_data(data_sourcer=ClassifierData(local_io=local_io),
-                                            years_requested=_years_requested, df_winners=df_winners,
-                                            df_close_games=df_close_games, df_timeouts_remaining=df_timeouts_remaining)
+    # TODO Classifier data: index=obs_id
+    # df_classifier_data = requester.get_data(data_sourcer=ClassifierData(local_io=local_io),
+    #                                         years_requested=_years_requested, df_winners=df_winners,
+    #                                         df_close_games=df_close_games, df_timeouts_remaining=df_timeouts_remaining)
 
     return
 
